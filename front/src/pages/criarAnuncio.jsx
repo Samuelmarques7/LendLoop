4e740411ced4418dafbf079b08fd5016c4b3cbce
danieleleticia <daniele.leticiaa810@gmail.com>
2026-03-26 10:29:01 -0300

    Criação do formúlario de informações

--- front/src/pages/criarAnuncio.jsx
+++ front/src/pages/criarAnuncio.jsx
@@ -4,8 +4,30 @@ function CriarAnuncio () // componente inicia com letra maiúscula
 {
     // Dentro do 'return' vai tudo que queremos mostrar na tela, como textos, imagens, etc.
     return (
-        <div>
-            <h1>Criar Anúncio</h1>
+        <div>   
+            <h1>Criar Novo Anúncio</h1> {/* 'h1' título maior */}
+            <h2>Detalhes do Anúncio</h2> {/* 'h2' título menor */}
+
+            <label>Título</label> {/* 'label' para descrever a que o campo se refere */}
+            <input placeholder="ex: Batedeira Arno" /> 
+
+            <label>Descrição</label>
+            <textarea placeholder="Descreva seu item em detalhes...." />
+            
+            <label>Categoria</label>
+            <select>
+                <option>Selecione</option>
+                <option>Ferramentas</option>
+                <option>Eletrônicos</option>
+            </select>
+
+            <label>Subcategoria</label>
+            <select>
+                <option>Selecione</option>              
+            </select>
+            
+            <button>Continuar</button>
+            
         </div>
     )
 }
