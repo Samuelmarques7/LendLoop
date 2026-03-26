a83cb5fc6989a5e638710d09ee448db78b8f6305
danieleleticia <daniele.leticiaa810@gmail.com>
2026-03-26 15:58:42 -0300

    implementa envio de formulário no botão 'Continuar'

--- front/src/pages/criarAnuncio.jsx
+++ front/src/pages/criarAnuncio.jsx
@@ -4,7 +4,21 @@ import { useState } from 'react'
 // 'function' estamos criando uma parte da interface (uma tela)
 function CriarAnuncio () // componente inicia com letra maiúscula
 {
-    // setTitulo não armazena uma variavel, mas sim uma função 
+    function handleSubmit (e) // 'e' é o evento de submit do formulario, essa função será chamada
+    {
+        // 'preventDefault' faz com que a página não recarregue
+        e.preventDefault()
+
+        console.log('Dados do formulário:')
+        console.log('Título:', titulo)
+        console.log('Descrição:', descricao)
+        console.log('Categoria:', categoria)
+        console.log('Subcategoria:', subcategoria)
+    }
+
+
+
+    // 'setTitulo' não armazena uma variavel, mas sim uma função 
     const [titulo, setTitulo] = useState("") 
     const [descricao, setDescricao] = useState("")
     const [categoria, setCategoria] = useState("")
@@ -16,38 +30,42 @@ function CriarAnuncio () // componente inicia com letra maiúscula
             <h1>Criar Novo Anúncio</h1> {/* 'h1' título maior */}
             <h2>Detalhes do Anúncio</h2> {/* 'h2' título menor */}
 
-            <label>Título</label> {/* 'label' para descrever a que o campo se refere */}
-            <input
-                placeholder="ex: Batedeira Arno" 
-                value = {titulo}
-                onChange = {(e) => setTitulo(e.target.value)}
-            />
+            <form onSubmit = {handleSubmit}>
+                <label>Título</label> {/* 'label' para descrever a que o campo se refere */}
+                <input
+                    placeholder="ex: Batedeira Arno" 
+                    value = {titulo}
+                    onChange = {(e) => setTitulo(e.target.value)}
+                />
 
-            <label>Descrição</label>
-            <textarea 
-                placeholder="Descreva seu item em detalhes...." 
-                value = {descricao}
-                onChange = {(e) => setDescricao(e.target.value)}
-            />
+                <label>Descrição</label>
+                <textarea 
+                    placeholder="Descreva seu item em detalhes...." 
+                    value = {descricao}
+                    onChange = {(e) => setDescricao(e.target.value)}
+                />
             
-            <label>Categoria</label>
-            <select
-                value = {categoria}
-                onChange = {(e) => setCategoria(e.target.value)}
-            >
-                <option>Selecione</option>
-                <option>Ferramentas</option>
-                <option>Eletrônicos</option>
-            </select>
+                <label>Categoria</label>
+                <select
+                    value = {categoria}
+                    onChange = {(e) => setCategoria(e.target.value)}
+                >
+                    <option>Selecione</option>
+                    <option value = 'ferramentas'>Ferramentas</option>
+                    <option value = 'eletronicos'>Eletrônicos</option>
+                </select>
 
-            <label>Subcategoria</label>
-            <select
-                value = {subcategoria}
-                onChange = {(e) => setSubcategoria(e.target.value)}>
-                <option>Selecione</option>              
-            </select>
+                <label>Subcategoria</label>
+                <select
+                    value = {subcategoria}
+                    onChange = {(e) => setSubcategoria(e.target.value)}
+                >
+                    <option>Selecione</option>
+                </select>
             
-            <button>Continuar</button>
+                {/* 'type=submit', pois submit é a convenção para botões de envio de formulario  */}
+                <button type="submit">Continuar</button>
+            </form>
             
         </div>
     )
