033df7057b12cebe5b586ac940392c2b59c420d3
danieleleticia <daniele.leticiaa810@gmail.com>
2026-03-26 11:10:39 -0300

    Conecta campos do forms ao estado

--- front/src/pages/criarAnuncio.jsx
+++ front/src/pages/criarAnuncio.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+
 // 'function' no react é um componente
 // 'function' estamos criando uma parte da interface (uma tela)
 function CriarAnuncio () // componente inicia com letra maiúscula
 {
+    // setTitulo não armazena uma variavel, mas sim uma função 
+    const [titulo, setTitulo] = useState("") 
+    const [descricao, setDescricao] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [subcategoria, setSubcategoria] = useState("")
+
     // Dentro do 'return' vai tudo que queremos mostrar na tela, como textos, imagens, etc.
     return (
         <div>   
@@ -9,20 +17,33 @@ function CriarAnuncio () // componente inicia com letra maiúscula
             <h2>Detalhes do Anúncio</h2> {/* 'h2' título menor */}
 
             <label>Título</label> {/* 'label' para descrever a que o campo se refere */}
-            <input placeholder="ex: Batedeira Arno" /> 
+            <input
+                placeholder="ex: Batedeira Arno" 
+                value = {titulo}
+                onChange = {(e) => setTitulo(e.target.value)}
+            />
 
             <label>Descrição</label>
-            <textarea placeholder="Descreva seu item em detalhes...." />
+            <textarea 
+                placeholder="Descreva seu item em detalhes...." 
+                value = {descricao}
+                onChange = {(e) => setDescricao(e.target.value)}
+            />
             
             <label>Categoria</label>
-            <select>
+            <select
+                value = {categoria}
+                onChange = {(e) => setCategoria(e.target.value)}
+            >
                 <option>Selecione</option>
                 <option>Ferramentas</option>
                 <option>Eletrônicos</option>
             </select>
 
             <label>Subcategoria</label>
-            <select>
+            <select
+                value = {subcategoria}
+                onChange = {(e) => setSubcategoria(e.target.value)}>
                 <option>Selecione</option>              
             </select>
             
